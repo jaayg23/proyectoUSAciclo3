bd0216b12d171f280b9249171590407f2b1ce97e
Jacobo Ayala <jaaygi01@gmail.com>
2021-09-30 18:38:08 -0500

    Implementacion en archivo javascript de las funciones para la seccion salones

--- misFunciones.js
+++ misFunciones.js
@@ -59,3 +59,62 @@ function guardarInformacionSalones(){
 		}
 	});
 }
+
+function traerInformacionClientes(){
+	$.ajax({    
+    url : 'https://g73664e6c770cfe-partyroom.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/client/client',
+	data: "{}",
+    type : 'GET',
+    dataType : 'json',
+    contentType: "application/json; charset=utf-8",
+  
+    success : function(respuesta) {
+		console.log(respuesta);
+		$("#resultadoClientes").empty();
+        let miTabla = '<table>';
+		for (i=0; i<respuesta.items.length; i++){
+			miTabla += '<tr>';
+	        miTabla += '<td>'+ respuesta.items[i].id+ '</td>'; 		
+	        miTabla += '<td>'+ respuesta.items[i].name+ '</td>'; 		
+	        miTabla += '<td>'+ respuesta.items[i].email+ '</td>'; 		
+	        miTabla += '<td>'+ respuesta.items[i].age+ '</td>'; 				
+			miTabla += '</tr>';
+	
+		}
+        miTabla += '</table>';
+	    $("#resultadoClientes").append(miTabla);    
+
+	},
+    error : function(xhr, status) {
+        alert('ha sucedido un problema:'+ status);
+    }
+});
+}
+
+function guardarInformacionClientes(){
+	let misDatos = {
+		id: $("#idClientes").val(),
+        name: $("#nameClientes").val(),
+        email: $("#email").val(),
+        age: $("#age").val(),
+	};
+	let datosJson = JSON.stringify(misDatos); 
+	$.ajax(    
+    'https://g73664e6c770cfe-partyroom.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/client/client',
+	{data: datosJson,
+    type : 'POST',
+    dataType : 'json',
+    contentType: "application/json; charset=utf-8",
+  
+    statusCode : {
+		201 :  function() {
+			alert("guardado!");
+			$("#idClientes").val("");
+			$("#nameClientes").val("");
+			$("#email").val("");
+			$("#age").val("");
+        	traerInformacionSalones();	
+			}
+		}
+	});
+}
