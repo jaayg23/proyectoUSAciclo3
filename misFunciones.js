353b932384faca17fbd35b09bab055b248542a6a
Jacobo Ayala <jaaygi01@gmail.com>
2021-10-20 09:26:44 -0500

    Ingreso de formulario para el html mensajes, con sus respectivas funciones

--- misFunciones.js
+++ misFunciones.js
@@ -118,3 +118,56 @@ function guardarInformacionClientes(){
 		}
 	});
 }
+
+function traerInformacionMensajes(){
+	$.ajax({    
+    url : 'https://g73664e6c770cfe-partyroom.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/message/message',
+	data: "{}",
+    type : 'GET',
+    dataType : 'json',
+    contentType: "application/json; charset=utf-8",
+  
+    success : function(respuesta) {
+		console.log(respuesta);
+		$("#resultadoMensajes").empty();
+        let miTabla = '<table>';
+		for (i=0; i<respuesta.items.length; i++){
+			miTabla += '<tr>';
+	        miTabla += '<td>'+ respuesta.items[i].id+ '</td>'; 		
+	        miTabla += '<td>'+ respuesta.items[i].messagetext+ '</td>'; 						
+			miTabla += '</tr>';
+	
+		}
+        miTabla += '</table>';
+	    $("#resultadoMensajes").append(miTabla);    
+
+	},
+    error : function(xhr, status) {
+        alert('ha sucedido un problema:'+ status);
+    }
+});
+}
+
+function guardarInformacionMensajes(){
+	let misDatos = {
+		id: $("#idMensajes").val(),
+        messagetext: $("#messagetext").val(),
+	};
+	let datosJson = JSON.stringify(misDatos); 
+	$.ajax(    
+    'https://g73664e6c770cfe-partyroom.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/message/message',
+	{data: datosJson,
+    type : 'POST',
+    dataType : 'json',
+    contentType: "application/json; charset=utf-8",
+  
+    statusCode : {
+		201 :  function() {
+			alert("guardado!");
+			$("#idMensajes").val("");
+			$("#messagetext").val("");
+        	traerInformacionMensajes();	
+			}
+		}
+	});
+}
