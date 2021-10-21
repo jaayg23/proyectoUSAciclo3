f3a270d12bcc97e3a17554fef00869847cdf7585
Jacobo Ayala <jaaygi01@gmail.com>
2021-10-21 16:41:33 -0500

    Ingresamos boton editar y actualizar con sus funcionalidades en salones.html

--- misFunciones.js
+++ misFunciones.js
@@ -16,7 +16,8 @@ function traerInformacionSalones(){
 	        miTabla += '<td>'+ respuesta.items[i].owner+ '</td>'; 		
 	        miTabla += '<td>'+ respuesta.items[i].capacity+ '</td>'; 		
 	        miTabla += '<td>'+ respuesta.items[i].category_id+ '</td>'; 		
-	        miTabla += '<td>'+ respuesta.items[i].name+ '</td>'; 		
+	        miTabla += '<td>'+ respuesta.items[i].name+ '</td>';
+			miTabla += '<td><button onclick="editarRegistroSalones('+respuesta.items[i].id+' )">Editar</button>' 		
 			miTabla += '</tr>';
 	
 		}
@@ -58,6 +59,61 @@ function guardarInformacionSalones(){
 			}
 		}
 	});
+}
+
+function editarRegistroSalones (id){
+	$.ajax({    
+	url : 'https://g73664e6c770cfe-partyroom.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/partyroom/partyroom/'+id,
+	data: "{}",
+	type : 'GET',
+	dataType : 'json',
+	contentType: "application/json; charset=utf-8",
+
+	success : function(respuesta) {
+		console.log(respuesta);
+		let miTabla = '<table>';
+		for (i=0; i<respuesta.items.length; i++){
+			$("#idSalones").val(respuesta.items[i].id);
+			$("#category_id").val(respuesta.items[i].category_id);
+			$("#owner").val(respuesta.items[i].owner);
+			$("#capacity").val(respuesta.items[i].capacity);
+			$("#nameSalones").val(respuesta.items[i].name);
+		}
+	},
+	error : function(xhr, status) {
+		alert('ha sucedido un problema:' +status + json);
+	}
+});
+}
+
+function actualizarInformacionSalones(){
+	let misDatos = {
+		id: $("#idSalones").val(),
+        category_id: $("#category_id").val(),
+        owner: $("#owner").val(),
+        capacity: $("#capacity").val(),
+        name: $("#nameSalones").val()
+	};
+	let datosJson = JSON.stringify(misDatos);
+	$.ajax(    
+	'https://g73664e6c770cfe-partyroom.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/partyroom/partyroom',
+	{data: datosJson,
+	type : 'PUT',
+	dataType : 'json',
+	contentType: "application/json; charset=utf-8",
+
+	statusCode : {
+		201 : function() {
+			alert("guardado!");
+			$("#idSalones").val("")
+			$("#category_id").val("");
+			$("#owner").val("");
+			$("#capacity").val("");
+			$("#nameSalones").val("");
+			traerInformacionSalones();
+		}
+	}
+});
 }
 
 function traerInformacionClientes(){
